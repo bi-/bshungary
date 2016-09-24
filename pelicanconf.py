63a3d7d5abf7a45c2c853eff48844bca3e7c147e
Imre Balassa <ib@aportadigital.com>
2016-09-24 20:46:54 +0200

    Remove duplicate STATIC_PATH

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,7 +43,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-STATIC_PATHS = ['images']	
+#STATIC_PATHS = ['images']	
 THEME = "./pelican-themes/bootstrap2"
 PLUGIN_PATHS = ['./pelican-plugins']
 PLUGINS = [ 'i18n_subsites']
